e636876ac8608611687d2fbff075377f086ce902
xuxiang <xuxiang@sensetime.com>
2021-04-22 20:31:37 +0800

    update

--- cpp/distributed/example.cpp
+++ cpp/distributed/example.cpp
@@ -1,6 +1,8 @@
 #include <c10d/FileStore.hpp>
 #include <c10d/TCPStore.hpp>
+#include <c10d/ProcessGroup.hpp>
 #include <c10d/ProcessGroupGloo.hpp>
+#include <c10d/ProcessGroupNCCL.hpp>
 
 using namespace c10d;
 
@@ -20,31 +22,37 @@ int main() {
     int rank = atoi(getenv("RANK"));
     int size = atoi(getenv("SIZE"));
 
+    std::shared_ptr<c10d::ProcessGroup> pg;
+
+    // communication store
     //auto store = c10::make_intrusive<FileStore>("/tmp/test", size);
     std::cout << "master addr: " << master_addr << std::endl;
     std::cout << "master port: " << master_port << std::endl;
     auto store = c10::make_intrusive<TCPStore>(master_addr, master_port, size, rank == 0);
 
-    c10d::ProcessGroupGloo::Options options;
-    options.timeout = std::chrono::milliseconds(100000);
+    std::string backend = "nccl";
+    
+    if (backend == "gloo") {
+        c10d::ProcessGroupGloo::Options options;
+        options.timeout = std::chrono::milliseconds(100000);
 
-    char* ifnameEnv = getenv("GLOO_SOCKET_IFNAME");
-    if (ifnameEnv) {
-      for (const auto& iface : split(',', ifnameEnv)) {
-        options.devices.push_back(
-            ::c10d::ProcessGroupGloo::createDeviceForInterface(iface));
-      }
-    } else {
-      // If no hostname is specified, this function looks up
-      // the machine's hostname and returns a device instance
-      // associated with the address that the hostname resolves to.
-      options.devices.push_back(
-         ::c10d::ProcessGroupGloo::createDefaultDevice());
+        char* ifnameEnv = getenv("GLOO_SOCKET_IFNAME");
+        if (ifnameEnv) {
+            for (const auto& iface : split(',', ifnameEnv)) {
+                options.devices.push_back(c10d::ProcessGroupGloo::createDeviceForInterface(iface))
+            }
+        } else {
+            // If no hostname is specified, this function looks up
+            // the machine's hostname and returns a device instance
+            // associated with the address that the hostname resolves to.
+            options.devices.push_back(c10d::ProcessGroupGloo::createDefaultDevice());
+        }
+
+        std::cout << "#devices: " << options.devices.size() << std::endl;
     }
 
-    std::cout << "#devices: " << options.devices.size() << std::endl;
-    ProcessGroupGloo pg(store, rank, size, options);
-
+    pg = std::make_shared<ProcessGroup>(new ProcessGroupGloo(store, rank, size, options));
+    
     const auto ntensors = 10;
     std::vector<at::Tensor> tensors;
     for(auto i = 0; i < ntensors; ++i) {
